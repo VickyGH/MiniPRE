9324d2bce4e5347e4a30131f1ead473f7cb94347
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2018-09-18 11:27:00 -0500

    d

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/Minipre';
-$config['base_url'] = 'https://mini-pre.herokuapp.com/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,8 +36,7 @@ $config['base_url'] = 'https://mini-pre.herokuapp.com/';
 | variable so that it is blank.
 |
 */
-//$config['index_page'] = '';
-$config['index_page'] = 'views/pages/inicio.php';
+$config['index_page'] = 'inicio';
 
 /*
 |--------------------------------------------------------------------------
@@ -382,8 +381,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-//$config['sess_save_path'] = NULL;
-$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
