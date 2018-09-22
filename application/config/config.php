173781585b71a68257c049e8f20a5759acefcaf0
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2018-09-22 13:29:05 -0500

    a

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'https://mini-pre.herokuapp.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'application/inicio';
 
 /*
 |--------------------------------------------------------------------------
